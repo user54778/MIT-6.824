e2c9784699fd3b16403394d90cf866dc83c5bd36
Ahron Pollak <ahronrocks51@gmail.com>
2025-07-15 17:37:16 -0400

    add leader election state

--- src/raft1/raft.go
+++ src/raft1/raft.go
@@ -1,10 +1,5 @@
+// Package Raft implements the Raft consensus algorithm for managing a replicated log.
 package raft
-
-// The file raftapi/raft.go defines the interface that raft must
-// expose to servers (or the tester), but see comments below for each
-// of these functions for more details.
-//
-// Make() creates a new raft peer that implements the raft interface.
 
 import (
 	//	"bytes"
@@ -16,11 +11,10 @@ import (
 	//	"6.5840/labgob"
 	"6.5840/labrpc"
 	"6.5840/raftapi"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
-
-// A Go object implementing a single Raft peer.
+// Type Raft is a Go object implementing a single Raft peer.
 type Raft struct {
 	mu        sync.Mutex          // Lock to protect shared access to this peer's state
 	peers     []*labrpc.ClientEnd // RPC end points of all peers
@@ -28,19 +22,45 @@ type Raft struct {
 	me        int                 // this peer's index into peers[]
 	dead      int32               // set by Kill()
 
-	// Your data here (3A, 3B, 3C).
-	// Look at the paper's Figure 2 for a description of what
-	// state a Raft server must maintain.
+	state RaftState // State a given Raft replica is in (Follower, Candidate, or Leader)
 
+	// NOTE: Persistent State: These fields must be stored to disk prior to responding to RPCs
+	currentTerm int        // Latest term server has seen (a monotonic integer)
+	votedFor    int        // The candidateID that received the vote in the currentTerm, or -1 if none.
+	logEntry    []LogEntry // log[] The log entries to replicate (3B onward)
+
+	// NOTE: Volatile State on all servers
+	// commitIndex
+	// lastApplied
+	// NOTE: Volatile state on leaders
+	// nextIndex[]
+	// matchIndex[]
+}
+
+// Type raftstate represents one of three states at any given point
+// a Raft replica may be in.
+type RaftState int
+
+const (
+	Follower RaftState = iota
+	Candidate
+	Leader
+)
+
+// Type LogEntry represents a Raft Log. A Raft log stores a state machine command
+// along with the term number when the entry was received by the Leader.
+type LogEntry struct {
+	Index   int // Position in the log
+	Term    int // Term number entry was received by the leader
+	Command any // State machine command
 }
 
 // return currentTerm and whether this server
 // believes it is the leader.
 func (rf *Raft) GetState() (int, bool) {
-
 	var term int
 	var isleader bool
-	// Your code here (3A).
+	// TODO: Your code here (3A).
 	return term, isleader
 }
 
@@ -61,7 +81,6 @@ func (rf *Raft) persist() {
 	// raftstate := w.Bytes()
 	// rf.persister.Save(raftstate, nil)
 }
-
 
 // restore previously persisted state.
 func (rf *Raft) readPersist(data []byte) {
@@ -90,32 +109,29 @@ func (rf *Raft) PersistBytes() int {
 	return rf.persister.RaftStateSize()
 }
 
-
 // the service says it has created a snapshot that has
 // all info up to and including index. this means the
 // service no longer needs the log through (and including)
 // that index. Raft should now trim its log as much as possible.
 func (rf *Raft) Snapshot(index int, snapshot []byte) {
 	// Your code here (3D).
-
 }
-
 
 // example RequestVote RPC arguments structure.
 // field names must start with capital letters!
 type RequestVoteArgs struct {
-	// Your data here (3A, 3B).
+	// TODO: Your data here (3A, 3B).
 }
 
 // example RequestVote RPC reply structure.
 // field names must start with capital letters!
 type RequestVoteReply struct {
-	// Your data here (3A).
+	// TODO: Your data here (3A).
 }
 
 // example RequestVote RPC handler.
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
-	// Your code here (3A, 3B).
+	// TODO: Your code here (3A, 3B).
 }
 
 // example code to send a RequestVote RPC to a server.
@@ -150,7 +166,6 @@ func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *Reques
 	return ok
 }
 
-
 // the service using Raft (e.g. a k/v server) wants to start
 // agreement on the next command to be appended to Raft's log. if this
 // server isn't the leader, returns false. otherwise start the
@@ -163,13 +178,12 @@ func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *Reques
 // if it's ever committed. the second return value is the current
 // term. the third return value is true if this server believes it is
 // the leader.
-func (rf *Raft) Start(command interface{}) (int, int, bool) {
+func (rf *Raft) Start(command any) (int, int, bool) {
 	index := -1
 	term := -1
 	isLeader := true
 
 	// Your code here (3B).
-
 
 	return index, term, isLeader
 }
@@ -194,11 +208,10 @@ func (rf *Raft) killed() bool {
 }
 
 func (rf *Raft) ticker() {
-	for rf.killed() == false {
-
-		// Your code here (3A)
+	for !rf.killed() {
+		// TODO: Your code here (3A)
+		//
 		// Check if a leader election should be started.
-
 
 		// pause for a random amount of time between 50 and 350
 		// milliseconds.
@@ -217,20 +230,33 @@ func (rf *Raft) ticker() {
 // Make() must return quickly, so it should start goroutines
 // for any long-running work.
 func Make(peers []*labrpc.ClientEnd, me int,
-	persister *tester.Persister, applyCh chan raftapi.ApplyMsg) raftapi.Raft {
+	persister *tester.Persister, applyCh chan raftapi.ApplyMsg,
+) raftapi.Raft {
 	rf := &Raft{}
 	rf.peers = peers
 	rf.persister = persister
 	rf.me = me
 
-	// Your initialization code here (3A, 3B, 3C).
+	/*
+		state RaftState // State a given Raft replica is in (Follower, Candidate, or Leader)
+
+		// NOTE: Persistent State: These fields must be stored to disk prior to responding to RPCs
+		currentTerm int        // Latest term server has seen (a monotonic integer)
+		votedFor    int        // The candidateID that received the vote in the currentTerm, or -1 if none.
+		logEntry    []LogEntry // log[] The log entries to replicate (3B onward)
+	*/
+	rf.state = Follower
+	rf.currentTerm = -1
+	rf.votedFor = -1
+	rf.logEntry = make([]LogEntry, 0)
 
 	// initialize from state persisted before a crash
 	rf.readPersist(persister.ReadRaftState())
 
+	Debug(logInfo, "S%d Follower first initialized", rf.me)
+
 	// start ticker goroutine to start elections
 	go rf.ticker()
-
 
 	return rf
 }
